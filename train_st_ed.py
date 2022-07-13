35ec1dd16a86153a118864706851dd120ba8791f
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-07-13 12:17:03 +0200

    u

--- train_st_ed.py
+++ train_st_ed.py
@@ -26,7 +26,7 @@ from PIL import Image
 from torchvision import transforms
 
 trans = transforms.Compose([
-        transforms.ToPILImage(),
+        #transforms.ToPILImage(),
         transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225]),
