ec241a97b270f950882420dbde2c1e737b2c129e
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-07-16 23:50:05 +0200

    u

--- train_st_ed.py
+++ train_st_ed.py
@@ -400,7 +400,7 @@ def execute_test_new(tag, data_dict):
                     loss = losses.gaze_angular_loss(pitchyaw_gt,pitchyaw_gen)
                     #print(loss)
                     angular_loss += loss.detach().cpu().numpy()
-                    print(angular_loss/num_images,loss,num_images)
+                    print(angular_loss/num_images,loss.detach().cpu().numpy(),num_images)
                 if index % 1 == 0:
                     img = np.concatenate([np.clip(((input_dict['image_a'].detach().cpu().permute(0, 2, 3, 1).numpy() +1) * 255.0/2.0),0,255).astype(np.uint8),np.clip(((input_dict['image_b'].detach().cpu().permute(0, 2, 3, 1).numpy() +1) * 255.0/2.0),0,255).astype(np.uint8),np.clip(((output_dict['image_b_hat'].detach().cpu().permute(0, 2, 3, 1).numpy()  +1) * 255.0/2.0),0,255).astype(np.uint8)],axis=2)
                     img = Image.fromarray(img[0])
