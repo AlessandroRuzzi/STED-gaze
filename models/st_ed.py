ea69f95ecadd7ae14dc9fab380e1e78e355b15da
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-10-28 11:49:36 +0200

    u

--- models/st_ed.py
+++ models/st_ed.py
@@ -38,7 +38,7 @@ trans = transforms.Compose([
         #transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
         #transforms.Normalize(mean=[0.485, 0.456, 0.406],
         #                     std=[0.229, 0.224, 0.225]),
-        transforms.Resize(size=(224,224)),
+        transforms.Resize(size=(128,128)),
     ])
 
 
