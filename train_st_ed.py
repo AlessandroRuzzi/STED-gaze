67e879764a3477999340aa03d1fc118f83ee3aeb
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-10-28 22:18:32 +0200

    u

--- train_st_ed.py
+++ train_st_ed.py
@@ -190,7 +190,7 @@ del saver
 
 if config.load_step != 0:
     #load_model(network, os.path.join(config.save_path, "checkpoints", str(config.load_step) + '.pt'),device)
-    load_model(network, os.path.join(config.save_path, "checkpoints", str(config.load_step) + '_orig.pt'),device)
+    load_model(network, os.path.join(config.save_path, "checkpoints", str(config.load_step) + '_orig_small.pt'),device)
     logging.info("Loaded checkpoints from step " + str(config.load_step))
 
 # Transfer on the GPU before constructing and optimizer
