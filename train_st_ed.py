5e9a9dd6ec3f075f6e817e57326cfd8d3175c0c7
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-10-31 16:06:37 +0100

    u

--- train_st_ed.py
+++ train_st_ed.py
@@ -185,12 +185,12 @@ _ = saver.load_last_checkpoint()
 del saver
 
 saver = CheckpointsManager(network.GazeHeadNet_train, config.gazenet_savepath,device)
-_ = saver.load_last_checkpoint(xgaze=True)
+_ = saver.load_last_checkpoint()
 del saver
 
 if config.load_step != 0:
     #load_model(network, os.path.join(config.save_path, "checkpoints", str(config.load_step) + '.pt'),device)
-    load_model(network, os.path.join(config.save_path, "checkpoints", str(config.load_step) + '_full.pt'),device)
+    load_model(network, os.path.join(config.save_path, "checkpoints", str(config.load_step) + '_orig.pt'),device)
     logging.info("Loaded checkpoints from step " + str(config.load_step))
 
 # Transfer on the GPU before constructing and optimizer
@@ -348,14 +348,14 @@ def execute_test(log, current_step):
     cam_matrix, cam_distortion, cam_translation, cam_rotation = load_cams()
 
 
-    #path = "sted/checkpoints/epoch_24_resnet_80_subj_ckpt.pth.tar"
-    path = "baseline_estimator_resnet/checkpoints/at_step_0140000.pth.tar"
-    #model = gaze_network_head().to(device)
-    model = GazeHeadResNet().to(device)
+    path = "sted/checkpoints/epoch_24_resnet_80_subj_ckpt.pth.tar"
+    #path = "baseline_estimator_resnet/checkpoints/at_step_0140000.pth.tar"
+    model = gaze_network_head().to(device)
+    #model = GazeHeadResNet().to(device)
 
     state_dict = torch.load(path, map_location=torch.device("cpu"))
-    #model.load_state_dict(state_dict=state_dict["model_state"])
-    model.load_state_dict(state_dict=state_dict)
+    model.load_state_dict(state_dict=state_dict["model_state"])
+    #model.load_state_dict(state_dict=state_dict)
     model.eval()
 
     print("Done")
